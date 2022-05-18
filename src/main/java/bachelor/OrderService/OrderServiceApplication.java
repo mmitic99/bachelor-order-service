762b828950e0228935bc78e9dafea8dfc532d598
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-05-18 17:36:53 +0200

    added feing client communication

--- src/main/java/bachelor/OrderService/OrderServiceApplication.java
+++ src/main/java/bachelor/OrderService/OrderServiceApplication.java
@@ -1,12 +1,17 @@
 package bachelor.OrderService;
 
+import bachelor.InventoryService.api.InventoryServiceApi;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient
-@EnableFeignClients(clients = {})
+@EnableFeignClients(clients = {InventoryServiceApi.class})
 @SpringBootApplication
 public class OrderServiceApplication {
 
@@ -14,4 +19,16 @@ public class OrderServiceApplication {
 		SpringApplication.run(OrderServiceApplication.class, args);
 	}
 
+}
+@RestController
+@RequestMapping("order")
+class SimpleBookRestController {
+
+	@Autowired
+	private InventoryServiceApi inventoryServiceApi;
+
+	@GetMapping(value = "", produces = "application/json")
+	public String get() {
+		return inventoryServiceApi.get("order");
+	}
 }
